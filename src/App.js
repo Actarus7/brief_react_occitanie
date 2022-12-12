4c1eaaea539fede4a4ebdcafb0e04cdf1d856811
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2022-12-12 16:26:55 +0100

    feat : création filter date

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState, } from "react";
 function App() {
 
   const [dataEvent, setDataEvent] = useState(undefined)
+  
+
 
   useEffect(() =>
   {
@@ -10,7 +12,11 @@ function App() {
     {
       const response = await fetch('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation');
       const responseJson = await response.json();
-      console.log(responseJson);
+      const filterDefault = new Date();
+      const filterMonthAddOne = filterDefault.setMonth(filterDefault.getMonth() + 1); 
+      const date = Number(String(filterDefault.getFullYear()) + String(filterDefault.getMonth()) + String(filterDefault.getDate()));
+      const a = Number(responseJson.records[0].fields.date_fin.replaceAll("-", "")); // replaceAll permet de supprimer tous les tirets et concatenne la date, Number transforme la date en string en number.
+
       setDataEvent(responseJson)
     }
     fetchData();
@@ -21,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>J'ai faim</h1>
+      <h1>Agenda Culturel Toulouse</h1>
       </header>
     </div>
   );
