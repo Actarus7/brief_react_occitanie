ab68103f838190741760dc0ef2afa79b037dbbe5
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2022-12-12 16:35:49 +0100

    feat : suite const date

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
     {
       const response = await fetch('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation');
       const responseJson = await response.json();
-      const filterDefault = new Date();
-      const filterMonthAddOne = filterDefault.setMonth(filterDefault.getMonth() + 1); 
-      const date = Number(String(filterDefault.getFullYear()) + String(filterDefault.getMonth()) + String(filterDefault.getDate()));
-      const a = Number(responseJson.records[0].fields.date_fin.replaceAll("-", "")); // replaceAll permet de supprimer tous les tirets et concatenne la date, Number transforme la date en string en number.
-
+      const filterDefault = new Date(); // date actuelle
+      const filterMonthAddOne = filterDefault.setMonth(filterDefault.getMonth() + 1); //date augmenté de 1 mois
+      const date = Number(String(filterMonthAddOne.getFullYear()) + String(filterMonthAddOne.getMonth()) + String(filterMonthAddOne.getDate())); // date au format nombre
+      const dateEvent = Number(responseJson.records[0].fields.date_fin.replaceAll("-", "")); // replaceAll permet de supprimer tous les tirets et concatenne la date, Number transforme la date en string en number.
+      if(date < dateEvent){
+        console.log(dateEvent);
+      }
       setDataEvent(responseJson)
     }
     fetchData();
