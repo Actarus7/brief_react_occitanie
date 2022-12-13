282058085232b8e0140e59702b137b3df09511a2
Steff <steff.1@gmail.com>
2022-12-13 15:45:32 +0100

    Feat fetchs mois en cours + mois à venir et concaténation dans un nouveau state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React, { useEffect, useState, } from "react";
 
 function App() {
-
+  const [dataEventM_1, setDataEventM_1] = useState([])
+  const [dataEventM_2, setDataEventM_2] = useState([])
   const [dataEvent, setDataEvent] = useState([])
 
 
-
   useEffect(() => {
-    /* const url = filtreDefault()
-    console.log(url); */
-    
-    async function fetchData(url) {
-      const response = await fetch('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=100&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_fin=2023%2F01');
+    async function fetchDataM_1() {
+      const date_1 = filtreDefaultM_1()
+      console.log(date_1.month);
+      const response = await fetch(`https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=300&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_fin=${date_1.year}%2F${date_1.month}`);
       const data = await response.json();
       const events = data.records;
-      setDataEvent(events)
-
+      setDataEventM_1(events)
     }
-
-    /* function filtreDefault() {
+    async function fetchDataM_2() {
+      const date_2 = filtreDefault_2()
+      const response = await fetch(`https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=300&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_fin=${date_2.year}%2F${date_2.month}`);
+      const data = await response.json();
+      const events = data.records;
+      setDataEventM_2(events)
+    }
+    function filtreDefaultM_1() {
       const dateNow = new Date()
       console.log(dateNow);
-      const datePlus1Mois = dateNow.setMonth(dateNow.getMonth()+1)
-      const year = datePlus1Mois/1000
-      const month = datePlus1Mois
-      const date = datePlus1Mois
-      const test = `${year}%2F${month}%2F${date}`
-      console.log(datePlus1Mois);
-      console.log(test);
-      return datePlus1Mois
-    } */
-    fetchData();
+      console.log(dateNow.getMonth())
+      const annee = dateNow.getFullYear()
+      const mois = dateNow.getMonth() + 1
+      const date = { year: annee, month: mois }
+      console.log(date);
+      return date
+    }
+    function filtreDefault_2() {
+      const dateNow = new Date()
+      let annee
+      let mois
+      if (dateNow.getMonth() === 11) {
+        annee = dateNow.getFullYear() + 1
+        mois = 1
+      }
+      else {
+        annee = dateNow.getFullYear()
+        mois = dateNow.getMonth() + 2
+      }
+      const date = { year: annee, month: mois };
+      return date
+    }
+    function concatDataEvents() {
+      const dataEventCopy = dataEventM_1.concat(dataEventM_2)
+      setDataEvent(dataEventCopy)
+    }
+    fetchDataM_1()
+    fetchDataM_2()
+    concatDataEvents()
 
   }, []);
+  console.log(dataEventM_1);
+  console.log(dataEventM_2);
   console.log(dataEvent);
+
 
   return (
     <div className="App">
