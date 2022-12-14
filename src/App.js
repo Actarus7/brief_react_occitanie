a8b06022a40fb19601b8f11ab73662737f7386cd
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-12-14 09:14:05 +0100

    feat ajout des marker sur la map en fonction des evenements présents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { } from './App.css'
 import Map from './components/map';
 import React, { useEffect, useState, } from "react";
+import { Marker } from 'react-leaflet';
 
 function App() {
 
@@ -11,31 +12,37 @@ function App() {
         /* const url = filtreDefault()
         console.log(url); */
 
-        async function fetchData(url) {
-            const response = await fetch('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=100&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_fin=2023%2F01');
-            const data = await response.json();
-            const events = data.records;
-            setDataEvent(events)
+        function fetchData(url) {
+            fetch('https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=100&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_fin=2023%2F01')
+                .then((response) => response.json())
+                .then((data) => {
+                    const events = data.records
+                    setDataEvent(events)
 
+                    console.log(dataEvent);
+                })
+
+
+            //console.log(dataEvent[0].fields.geo_point);
         }
 
-/* function filtreDefault() {
-    const dateNow = new Date()
-    console.log(dateNow);
-    const datePlus1Mois = dateNow.setMonth(dateNow.getMonth()+1)
-    const year = datePlus1Mois/1000
-    const month = datePlus1Mois
-    const date = datePlus1Mois
-    const test = `${year}%2F${month}%2F${date}`
-    console.log(datePlus1Mois);
-    console.log(test);
-    return datePlus1Mois
-        } */
-    fetchData();
+        /* function filtreDefault() {
+            const dateNow = new Date()
+            console.log(dateNow);
+            const datePlus1Mois = dateNow.setMonth(dateNow.getMonth()+1)
+            const year = datePlus1Mois/1000
+            const month = datePlus1Mois
+            const date = datePlus1Mois
+            const test = `${year}%2F${month}%2F${date}`
+            console.log(datePlus1Mois);
+            console.log(test);
+            return datePlus1Mois
+                } */
+        fetchData();
 
     }, []);
 
-    console.log(dataEvent);
+
 
     return (
         <div className="App">
@@ -43,12 +50,13 @@ function App() {
                 <h1>Agenda Culturel Toulouse</h1>
                 <div className='container'>
                     <div id="map">
-                        <Map ></Map>
+                    <Map data={dataEvent}/>
                     </div>
                 </div>
             </header>
         </div>
     );
 }
+
 
 export default App;
--- src/components/map.js
+++ src/components/map.js
@@ -1,21 +1,22 @@
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 import { } from '../App.css'
 
-export default function Map() {
-
-
+export default function Map(props) {
+console.log(props.data);
     return (
-        <MapContainer style={{ width: '60vh', height: '60vh' }} center={[43.60306511042458, 1.4367906616822803]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer style={{ width: '60vh', height: '60vh' }} center={[43.60306511042458, 1.4367906616822803]} zoom={12} scrollWheelZoom={false}>
 
             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[43.570032188000845, 1.4021166257656503]}>
-                <Popup>
-                    Venez observer la dignité de Stephane !
-                </Popup>
-            </Marker>
+            {props.data.length && (props.data?.map((elem, i) => 
+                <Marker key={i} position={elem.fields?.geo_point}>
+                    
+                </Marker>
+                ))
+            }
+
         </MapContainer>)
 }
 
-//
+//{dataEvent.map((elem, i) => <Marker position={elem[i].fields.geo_point}/>)}
