b11a6330c54b3d0c9375f77fb15f6cd20233b628
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-12-15 14:25:34 +0100

    fix responsive div a propos

--- src/components/aPropos.js
+++ src/components/aPropos.js
@@ -1,15 +1,16 @@
 export default function Apropos() {
 
 
-    return (<div className="border border-3 border-danger rounded-4 position-absolute top-50 start-50 translate-middle p-4">
+    return (
+    <div className="border border-3 border-danger rounded-4 p-4 mt-5 mb-4">
 
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Arcu bibendum at varius vel. Justo donec enim diam vulputate ut pharetra. Faucibus in ornare quam viverra orci sagittis eu volutpat.
-        Nullam ac tortor vitae purus. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. 
-        Sit amet cursus sit amet dictum. Nunc id cursus metus aliquam eleifend. Maecenas ultricies mi eget mauris pharetra et. 
-        Vitae turpis massa sed elementum. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. 
-        Adipiscing elit duis tristique sollicitudin. Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. 
-        Suspendisse in est ante in nibh. Nulla aliquet porttitor lacus luctus. Semper auctor neque vitae tempus quam.</p>
+        Arcu bibendum at varius vel.<br/> Justo donec enim diam vulputate ut pharetra. Faucibus in ornare quam viverra orci sagittis eu volutpat.
+        Nullam ac tortor vitae purus. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec.<br/> 
+        Sit amet cursus sit amet dictum. Nunc id cursus metus aliquam eleifend. Maecenas ultricies mi eget mauris pharetra et.<br/> 
+        Vitae turpis massa sed elementum. <br/> Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. 
+        Adipiscing elit duis tristique sollicitudin.<br/> Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. 
+        Suspendisse in est ante in nibh. Nulla aliquet porttitor lacus luctus.<br/> Tout ça pour dire que le gras c'est la vie.</p>
     
     </div>)
 }
