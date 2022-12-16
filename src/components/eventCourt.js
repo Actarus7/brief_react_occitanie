074cd623adaa8c3ec16f6cbe47ced9e389c717dd
Steff <steff.1@gmail.com>
2022-12-16 10:42:53 +0100

    Fix contour image

--- src/components/eventCourt.js
+++ src/components/eventCourt.js
@@ -5,10 +5,10 @@ export default function EventCourt(props) {
     //let typeEvent = props.handleGetTypes
     //console.log(typeEvent)
     let indexElem = 8
-    const [indexPage, setN] = useState(2)
-    let cardEvent = props.data.filter((elem, index) => index > 7 && index < indexPage * indexElem).map((elm, i) => {
+    const [indexPage, setN] = useState(1)
+    let cardEvent = props.data.filter((elem, index) => index >= 0 && index < indexPage * indexElem).map((elm, i) => {
         return <div className="card d-flex m-1 p-1 flex-fill border border-danger border-2 rounded-5" style={{ width: 18 + 'rem' }} key={i} onclick={() => props.setPage("details")}>
-            <img src={`/img/${getImageType(elm.fields.type_de_manifestation)}.jpg`} className="card-img-top" alt="..." />
+            <img src={`/img/${getImageType(elm.fields.type_de_manifestation)}.jpg`} className="card-img-top rounded-5" alt="..." />
 
             <div className="card-body">
                 <h6 className="card-title text-center">{elm.fields.type_de_manifestation}</h6>
