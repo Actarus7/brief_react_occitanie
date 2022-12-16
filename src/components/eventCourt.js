7fa3fe05bd4c9b777f7409c0f0ec85575305b961
TB <terry.boeuf@hotmail.fr>
2022-12-16 13:04:53 +0100

    add: input date

--- src/components/eventCourt.js
+++ src/components/eventCourt.js
@@ -7,7 +7,7 @@ export default function EventCourt(props) {
     let indexElem = 8
     const [indexPage, setN] = useState(1)
     let cardEvent = props.data.filter((elem, index) => index >= 0 && index < indexPage * indexElem).map((elm, i) => {
-        return <div className="card d-flex m-1 p-1 flex-fill border border-danger border-2 rounded-5" style={{ width: 18 + 'rem' }} key={i} onclick={() => props.setPage("details")}>
+        return <div className="card d-flex m-1 p-0 flex-fill border border-danger border-2 rounded-5" style={{ width: 18 + 'rem' }} key={i} onclick={() => props.setPage("details")}>
             <img src={`/img/${getImageType(elm.fields.type_de_manifestation)}.jpg`} className="card-img-top rounded-5" alt="..." />
 
             <div className="card-body">
@@ -29,40 +29,40 @@ export default function EventCourt(props) {
         else if (type.includes('Insolite')) { image = 'insolite' }
         else if (type.includes('Sports')) { image = 'sport' }
         else if (type.includes('Son et lumière')) { image = 'culturel' }
-        else {image = 'autre'}
+        else { image = 'autre' }
         return image
     }
 
     // affichage
     return (
-        
-            <div className='row border border-danger border-3 rounded-4 p-2 mb-4 mt-4'>
-                <h5>Résultats :</h5>
-                {cardEvent}
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center mt-4">
-                        <li class="page-item">
-                            <a class="page-link bg-warning text-dark" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&lt;</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage}</a></li>
-                        <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage + 1}</a></li>
-                        <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage + 2}</a></li>
-                        <li class="page-item">
-                            <a class="page-link bg-warning text-dark" href="#" aria-label="Next">
-                                <span aria-hidden="true">&gt;</span>
-                            </a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link bg-warning text-dark" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        
+
+        <div className='row border border-danger border-3 rounded-4 p-2 mb-4 mt-4'>
+            <h5>Résultats :</h5>
+            {cardEvent}
+            <nav aria-label="Page navigation">
+                <ul class="pagination justify-content-center mt-4">
+                    <li class="page-item">
+                        <a class="page-link bg-warning text-dark" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&lt;</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage}</a></li>
+                    <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage + 1}</a></li>
+                    <li class="page-item"><a class="page-link page-link bg-warning text-dark" href="#">{indexPage + 2}</a></li>
+                    <li class="page-item">
+                        <a class="page-link bg-warning text-dark" href="#" aria-label="Next">
+                            <span aria-hidden="true">&gt;</span>
+                        </a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link bg-warning text-dark" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
 
     )
 }
