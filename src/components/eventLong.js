81dfab1146cdb67f001529c27d1c6c0b2ef76f24
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-12-19 10:59:12 +0100

    fix infos sans donnée n'apparaissent plus eventLong. feat image sur EvenLong.

--- src/components/eventLong.js
+++ src/components/eventLong.js
@@ -3,10 +3,24 @@ import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 
 export default function EventLong({ data }) {
 
+    function getImageType(type) {
+        let image = ""
+        if (type.includes('Culturelle')) { image = 'culturel' }
+        else if (type.includes('Musique')) { image = 'musique' }
+        else if (type.includes('Manifestation commerciale')) { image = 'insolite' }
+        else if (type.includes('Danse')) { image = 'danse' }
+        else if (type.includes('Traditions et folklore')) { image = 'culturel' }
+        else if (type.includes('Insolite')) { image = 'insolite' }
+        else if (type.includes('Sports')) { image = 'sport' }
+        else if (type.includes('Son et lumière')) { image = 'culturel' }
+        else { image = 'autre' }
+        return image
+    }
+
 
     return (<><div className="d-flex border border-danger border-2 rounded-5 flex-wrap mt-5 flex-column mb-4 ">
         <div>
-            {/* {<img src="{imgCatEvent}" alt="{data.type_de_manifestation}" />} */}
+            <img className=" card-img-top img-fluid img rounded-5" src={`/img/${getImageType(data.type_de_manifestation)}.jpg`} alt={data.type_de_manifestation} />
             <h1 className="text-center">{data.type_de_manifestation}</h1>
             <h2 className="text-center">{data.nom_de_la_manifestation}</h2>
         </div>
@@ -15,23 +29,23 @@ export default function EventLong({ data }) {
         </div>
         <div className="d-flex border border-danger border-2 rounded-5 flex-column p-3 me-5 large ms-5 mb-3">
             <div className="flex-shrink-1 ">
-                <p>Horaires: {data.dates_affichage_horaires}</p>
+                {data.dates_affichage_horaires !== undefined ? <p>Horaires: {data.dates_affichage_horaires}</p> : ""}
             </div>
             <div>
-                <p>Adresse: {data.lieu_adresse_1}
+                {data.lieu_adresse_1 || data.lieu_adresse_2 !== undefined ? <p>Adresse: {data.lieu_adresse_1}
                     <br />
-                    {data.lieu_adresse_2}</p>
+                    {data.lieu_adresse_2}</p> : ""}
             </div>
             <div>
-                <p>Tarifs : {data.tarif_normal}</p>
+                {data.tarif_normal !== undefined ? <p>Tarifs : {data.tarif_normal}</p> : ""}
             </div>
         </div>
 
         <div className="d-flex border border-danger border-2 rounded-5 flex-column p-3 me-5 large ms-5 mb-4">
-            <p>Reservation e-mail: {data.reservation_email}</p>
-            <p>Reservation via le site: {data.reservation_site_internet}</p>
-            <p>Reservation par téléphone: {data.reservation_telephone}</p>
-            <p>Accès: {data.station_metro_tram_a_proximite}</p>
+            {data.reservation_email !== undefined ? <p>Reservation e-mail: {data.reservation_email} </p>:"" }
+            {data.reservation_site_internet !== undefined ? <p>Reservation via le site: {data.reservation_site_internet}</p> : ""}
+            {data.reservation_telephone !== undefined ? <p>Reservation par téléphone: {data.reservation_telephone}</p> : ""}
+            {data.station_metro_tram_a_proximite !== undefined ? <p>Accès: {data.station_metro_tram_a_proximite}</p> : ""}
         </div>
 
 
